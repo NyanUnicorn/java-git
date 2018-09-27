d7cf0856c5e2c9ac662ca119c77b55b311a1f0ca
Phillip Redfern <philllip347@gmail.com>
2018-09-27 13:48:05 +0200

    updt

--- JeuVieTP1/src/jeuxvie/Principale.java
+++ JeuVieTP1/src/jeuxvie/Principale.java
@@ -1,5 +1,7 @@
 package jeuxvie;
 
+import controleur.Controleur;
+
 public class Principale {
 
 	public Principale() {
@@ -15,6 +17,10 @@ public class Principale {
 		System.out.println(b0);
 		System.out.println(b1);
 		System.out.println(b2);
+		Controleur ct = new Controleur();
+		ct.setBestiole(b0, 50, 50);
+		ct.setBestiole(b1, 10, 50);
+		ct.setBestiole(b2, 50, 40);
 	}
 
 }
