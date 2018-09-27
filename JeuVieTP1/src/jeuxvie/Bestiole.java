4c460c8d6db939047139fe4e328b950952b33235
Phillip Redfern <philllip347@gmail.com>
2018-09-27 11:51:46 +0200

    initial

---
+++ JeuVieTP1/src/jeuxvie/Bestiole.java
@@ -0,0 +1,21 @@
+package jeuxvie;
+
+public class Bestiole {
+	
+	
+	private int number;
+	private String couleur;
+
+	
+	
+	public Bestiole(String couleur) {
+		super();
+		this.couleur = couleur;
+	}
+
+	
+	public Bestiole() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ JeuVieTP1/src/jeuxvie/Principale.java
@@ -0,0 +1,14 @@
+package jeuxvie;
+
+public class Principale {
+
+	public Principale() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
